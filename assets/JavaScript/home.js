a99e92984e33ac7bc87f8574bedddabfa6a1af5c
Mahmoud Mohamed <mahmoudmahmed1499@gmail.com>
2023-02-24 15:40:57 +0200

    scrollUp buttun and header scroller progress added

--- assets/JavaScript/home.js
+++ assets/JavaScript/home.js
@@ -1,1 +1,28 @@
-// Footer ///////////////////
+// ///////// START Header scoller ///////////////////
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
+// ///////// END Header scoller ///////////////////
+
+// /////////Footer ///////////////////
+
+/////// START SCROLL UP BUTTON ////////////
+let upSpan = document.querySelector(".up");
+
+window.onscroll = () =>
+  this.scrollY >= 500
+    ? upSpan.classList.add("show")
+    : upSpan.classList.remove("show");
+
+upSpan.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+///////END SCROLL UP BUTTON ////////////
