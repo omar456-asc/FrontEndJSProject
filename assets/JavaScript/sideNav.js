cd6d920dfbbff837d05a17b320f691bea19bd641
sara hussam <sarahussam203@gmail.com>
2023-02-26 17:46:41 +0200

    Card-sideNav

---
+++ assets/JavaScript/sideNav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+   
+  }
