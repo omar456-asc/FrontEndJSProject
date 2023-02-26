cfa180be1409ce977bed083a357151dacd88700e
Omar Walid <omo_1712@yahoo.com>
2023-02-26 16:41:00 +0200

    Products Filter Added

--- assets/JavaScript/home.js
+++ assets/JavaScript/home.js
@@ -15,7 +15,6 @@ fetch("../JSON/products.json")
     return myData;
   })
   .then((myData) => {
-    // console.log(myData.products[0].id);
     if (cardContainer) {
       document.getElementById("card-container").replaceWith(cardContainer);
       return;
@@ -24,38 +23,97 @@ fetch("../JSON/products.json")
     cardContainer = document.getElementById("card-container");
     myData.products.forEach((data) => {
       createCard(data);
+      filterProduct("all");
+
     });
   });
-
 //bootstrap card
 let cardContainer;
 
 let createCard = (data) => {
-  let card = document.createElement("div");
-  card.className = "col-lg-4 col-md-6 p-2 card shadow cursor-pointer";
-
-  let cardBody = document.createElement("div");
-  cardBody.className = "card-body";
-
-  let fullName = document.createElement("h5");
-  fullName.innerText = data.id + " " + data.title;
-  fullName.className = "card-fullName";
-
-  let img = document.createElement("img");
-  img.src = data.images[1];
-
-  let email = document.createElement("div");
-  email.innerText = data.email;
-  email.className = "card-color";
-
-  cardBody.appendChild(fullName);
-  cardBody.appendChild(img);
-  cardBody.appendChild(email);
-  card.appendChild(cardBody);
-  cardContainer.appendChild(card);
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", data.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", data.images[0]);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = data.title.toUpperCase();
+    container.appendChild(name);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + data.price;
+    container.appendChild(price);
+  
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  
 };
+ //parameter passed from button (Parameter same as category)
+ function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
 
-// /////////// end products data
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+// /////////// ######################end products data################################//
 /////// START SCROLL UP BUTTON ////////////
 let upSpan = document.querySelector(".up");
 
@@ -71,3 +129,4 @@ upSpan.onclick = () => {
   });
 };
 ///////END SCROLL UP BUTTON ////////////
+
