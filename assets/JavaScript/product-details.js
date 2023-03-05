dd251ec2eec486682e7b5bee716ba13d1d20152f
sara hussam <sarahussam203@gmail.com>
2023-03-05 15:46:58 +0200

    details-page

--- assets/JavaScript/product-details.js
+++ assets/JavaScript/product-details.js
@@ -1,8 +1,32 @@
+if( JSON.parse(localStorage.getItem("details")) !=null){
+    let details= JSON.parse(localStorage.getItem("details"))
+    
+    console.log(details)
+  
+  
+
+var productname=document.querySelector(".product-name")
+productname.innerHTML=details.name
+var productprice=document.querySelector(".product-price")
+productprice.innerHTML=`$${details.price}`
+var productbrand=document.querySelector(".brand")
+productbrand.innerHTML=`${details.brand}`
+var productcategory=document.querySelector(".category")
+productcategory.innerHTML=`${details.category}`
+var productdesc=document.querySelector(".description")
+productdesc.innerHTML=`${details.description}`
+var discountPercentage=document.querySelector(".discountPercentage")
+discountPercentage.innerHTML=`${details.discountPercentage}`
+var rating=document.querySelector(".rating")
+rating.innerHTML=`${details.rating}`
+var stock=document.querySelector(".stock")
+stock.innerHTML=`${details.stock}`
 var largeimg = document.querySelector(".largeimg");
 var img = document.querySelectorAll(".activeImg div img");
-
+largeimg.src=`${details.largeimg}`
+console.log(largeimg.src)
 for (let i = 0; i < img.length; i++) {
-
+    img[i].src=`${details.images[i]}`
     img[i].onclick = function () {
         img.forEach(element => {
             element.classList.remove("active");
@@ -11,4 +35,17 @@ for (let i = 0; i < img.length; i++) {
 
         });
     }
-}
+}}
+
+function openNav() {
+    if( document.getElementById("mySidenav").style.width == "320px"){
+      document.getElementById("mySidenav").style.width = "0";
+    }
+    else{
+      document.getElementById("mySidenav").style.width = "320px";
+    }
+  }
+  
+  function closeNav() {
+   document.getElementById("mySidenav").style.width = "0"; 
+  }
