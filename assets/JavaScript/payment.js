59aa287fc68aa6dbb59ab7523d17441a9904737f
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-03-05 15:21:36 +0200

    loop products

--- assets/JavaScript/payment.js
+++ assets/JavaScript/payment.js
@@ -31,3 +31,39 @@ infoBtn.onclick = function(){
     payBtn.disabled = true;
     payBtn.classList.remove('bold');
 }
+
+
+
+// loop cart
+
+var items = [
+    ['Logitek', 'Rp 60.000,00', 'Logitek Keyboard', 'logitek.jpeg'],
+    ['MSI', 'Rp 300.000,00', 'MSI Keyboard', 'msi.jpeg'],
+    ['Genius', 'Rp 50.000,00', 'Genius Mouse', 'genius.jpeg'],
+    ['Jerry', 'Rp 30.000,00', 'Jerry Mouse', 'jerry.jpeg']
+  ]
+  
+  let content = '';
+  
+  items.forEach(p => {
+    content += `
+            <div class="row g-0">
+                  <div class="col-md-4">
+                    <img 
+                      src="${p[3]}"
+                      class="img-fluid rounded-start"
+                    />
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title" id="productName">${p[0]}</h5>
+                      <p class="card-text" >
+                        <small class="text-muted" id="productPrice">${p[2]}</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+    `
+  });
+  
+  document.querySelector("#shop").innerHTML = content;
