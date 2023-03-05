fadb54ef0a048556998ab6eb2dd9f37ebc6f8e09
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-03-05 14:55:47 +0200

    paymentcss3

--- assets/JavaScript/payment.js
+++ assets/JavaScript/payment.js
@@ -6,12 +6,15 @@ let cashBtn = document.getElementById('cashBtn');
 let visaForm = document.getElementById('visaForm');
 let infoBtn = document.getElementById('infoBtn');
 let payBtn = document.getElementById('payBtn');
+let cashMsg = document.getElementById('cashMsg');
 
 visaBtn.onclick = function(){
     visaForm.classList.remove('hide');
+    cashMsg.classList.add('hide');
 }
 cashBtn.onclick = function(){
     visaForm.classList.add('hide');
+    cashMsg.classList.remove('hide');
 }
 
 btn.onclick = function(){
